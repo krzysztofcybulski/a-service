e243b24cec3a06bfe206c7d9f4dfdb98b1667e16
Krzysztof Cybulski <krzysztofpcy@gmail.com>
2018-07-07 11:49:21 +0200

    Basic app skeleton Frontend (Angular) Backend (Spring Boot)

---
+++ backend/src/main/java/me/veloxdigitis/aservice/Application.java
@@ -0,0 +1,12 @@
+package me.veloxdigitis.aservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
